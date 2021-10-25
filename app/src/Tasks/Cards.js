8fd3aa336bab959a2c0f9b19bced6dde2fbf636b
Sarah Graywood <smgraywood@gmail.com>
2021-10-25 14:43:40 -0700

    creation of form

---
+++ app/src/Tasks/Cards.js
@@ -0,0 +1,129 @@
+import React from "react";
+import "./cards.css";
+
+function Cards() {
+  return (
+    <section className="card-wrapper">
+      <div>
+        <div className="card">
+          <div className="top1"></div>
+          <div className="card-text">
+            Honeysuckle <br /> #CF5476 <br />
+            2011
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card">
+          <div className="top2"></div>
+          <div className="card-text">
+            Tangerine Tango <br /> #DD4124 <br />
+            2012
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top3"></div>
+          <div className="card-text">
+            Emerald <br /> #009B77 <br />
+            2013
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top4"></div>
+          <div className="card-text">
+            Radiant Orchid <br /> #B163A3 <br />
+            2014
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top5"></div>
+          <div className="card-text">
+            Marsala <br /> #955251 <br />
+            2015
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top6"></div>
+          <div className="card-text">
+            Serenity <br /> #B3CEE5 <br />
+            2016
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top7"></div>
+          <div className="card-text">
+            Rose Quartz <br /> #F7CAC9 <br />
+            2016
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top8"></div>
+          <div className="card-text">
+            Greenery <br /> #88B04B <br />
+            2017
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top9"></div>
+          <div className="card-text">
+            Ultra Violet <br /> #604c8d <br />
+            2018
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top10"></div>
+          <div className="card-text">
+            Living Coral <br /> #ff6f61 <br />
+            2019
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top11"></div>
+          <div className="card-text">
+            Classic Blue <br /> #34558b <br />
+            2020
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top12"></div>
+          <div className="card-text">
+            Ultimate Gray <br /> #959A9C <br />
+            2021
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top13"></div>
+          <div className="card-text">
+            Illuminating <br /> #F5DF4D <br />
+            2021
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export { Cards };
---
+++ app/src/Tasks/Form.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+
+function Form({ Post, addPost }) {
+  const [color_name, setColor_name] = useState("");
+  const [release_year, setRelease_year] = useState("");
+  const [image_url, setImage_url] = useState("");
+  const [image_description, setImage_description] = useState("");
+  const [instagram_handle, setInstagram_handle] = useState("");
+
+  const handleColor_name = (e) => {
+    setColor_name(e.target.value);
+  };
+
+  const handleRelease_year = (e) => {
+    setRelease_year(e.target.value);
+  };
+
+  const handleImage_url = (e) => {
+    setImage_url(e.target.value);
+  };
+
+  const handleImage_description = (e) => {
+    setImage_description(e.target.value);
+  };
+
+  const handleInstagram_handle = (e) => {
+    setInstagram_handle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+    addPost({
+      color_name: color_name,
+      release_year: release_year,
+      image_url: image_url,
+      image_description: image_description,
+      instagram_handle: instagram_handle,
+    });
+  };
+
+  return (
+    <div>
+      <br />
+      <form className="pride_form">
+        <h3 id="form">Show Your Pantone Pride!</h3>
+        <label>
+          Color Name:{" "}
+          <input
+            type="text"
+            name="color_name"
+            value={color_name}
+            // onSubmit={handleSubmit}
+            onChange={handleColor_name}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          Release Year:{" "}
+          <input
+            type="text"
+            name="release_year"
+            value={release_year}
+            // onSubmit={handleSubmit}
+            onChange={handleRelease_year}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          Image URL:{" "}
+          <input
+            type="text"
+            name=" image_url"
+            value={image_url}
+            // onSubmit={handleSubmit}
+            onChange={handleImage_url}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          Image Description:{" "}
+          <input
+            type="text"
+            name="image_description"
+            value={image_description}
+            // onSubmit={handleSubmit}
+            onChange={handleImage_description}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          Instagram Handle:{" "}
+          <input
+            type="text"
+            name="instagram_handle"
+            value={instagram_handle}
+            // onSubmit={handleSubmit}
+            onChange={handleInstagram_handle}
+          />
+        </label>{" "}
+        <br />
+        <br />
+        <button id="addpost" onClick={handleSubmit}>
+          Add Post
+        </button>
+        {/* <h5>
+          Player 1: {state.Player_Name} {state.X_or_O}
+        </h5> */}
+      </form>
+    </div>
+  );
+}
+export { Form };
