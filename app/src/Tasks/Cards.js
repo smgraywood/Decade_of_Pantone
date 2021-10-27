4ddd47c43ba9c7bb48e3a339b9e194d3f30bf2e3
Sarah Graywood <smgraywood@gmail.com>
2021-10-27 10:51:21 -0700

    fixing things that somehow got deleted

---
+++ app/src/Tasks/Cards.js
@@ -0,0 +1,129 @@
+import React from "react";
+import "./cards.css";
+
+function Cards() {
+  return (
+    <section className="card-wrapper">
+      <div>
+        <div className="card">
+          <div className="top2011"></div>
+          <div className="card-text">
+            Honeysuckle <br /> #CF5476 <br />
+            2011
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card">
+          <div className="top2012"></div>
+          <div className="card-text">
+            Tangerine Tango <br /> #DD4124 <br />
+            2012
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2013"></div>
+          <div className="card-text">
+            Emerald <br /> #009B77 <br />
+            2013
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2014"></div>
+          <div className="card-text">
+            Radiant Orchid <br /> #B163A3 <br />
+            2014
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2015"></div>
+          <div className="card-text">
+            Marsala <br /> #955251 <br />
+            2015
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2016_1"></div>
+          <div className="card-text">
+            Serenity <br /> #B3CEE5 <br />
+            2016
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2016_2"></div>
+          <div className="card-text">
+            Rose Quartz <br /> #F7CAC9 <br />
+            2016
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2017"></div>
+          <div className="card-text">
+            Greenery <br /> #88B04B <br />
+            2017
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2018"></div>
+          <div className="card-text">
+            Ultra Violet <br /> #604c8d <br />
+            2018
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2019"></div>
+          <div className="card-text">
+            Living Coral <br /> #ff6f61 <br />
+            2019
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2020"></div>
+          <div className="card-text">
+            Classic Blue <br /> #34558b <br />
+            2020
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2021_1"></div>
+          <div className="card-text">
+            Ultimate Gray <br /> #959A9C <br />
+            2021
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="top2021_2"></div>
+          <div className="card-text">
+            Illuminating <br /> #F5DF4D <br />
+            2021
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+//something to commit
+export { Cards };
--- app/src/Tasks/index.js
+++ app/src/Tasks/index.js
@@ -2,23 +2,32 @@ import * as React from "react";
 
 import * as apiClient from "../apiClient";
 
+import { Cards } from "./Cards";
+import { Form } from "./Form";
+import "./cards.css";
 import styles from "./styles.module.scss";
 
 const Tasks = () => {
   const [tasks, setTasks] = React.useState([]);
 
   const loadTasks = async () => setTasks(await apiClient.getTasks());
-  const addTask = (task) => apiClient.addTask(task).then(loadTasks);
+  const addPride = (task) => apiClient.addTask(task).then(loadTasks);
 
   React.useEffect(() => {
     loadTasks();
   }, []);
 
   return (
-    <section>
-      <TaskList {...{ tasks }} />
-      <AddTask {...{ addTask }} />
-    </section>
+    <>
+      <h1 className="title">A Decade of Pantone</h1>
+      <section>
+        <Cards />
+        {/* <TaskList {...{ tasks }} /> */}
+        <form className="pride_form">
+          <AddPride {...{ addPride }} />
+        </form>
+      </section>
+    </>
   );
 };
 
@@ -30,7 +39,7 @@ const TaskList = ({ tasks }) => (
   </ul>
 );
 
-const AddTask = ({ addTask }) => {
+const AddPride = ({ addPride }) => {
   const [task, setTask] = React.useState("");
 
   const canAdd = task !== "";
@@ -38,21 +47,22 @@ const AddTask = ({ addTask }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (canAdd) {
-      addTask(task);
+      addPride(task);
       setTask("");
     }
   };
 
   return (
-    <form {...{ onSubmit }}>
-      <label>
-        New task:{" "}
-        <input onChange={(e) => setTask(e.currentTarget.value)} value={task} />
-      </label>
-      <button disabled={!canAdd} className={styles.button}>
-        Add
-      </button>
-    </form>
+    // <form {...{ onSubmit }}>
+    //   <label>
+    //     New task:{" "}
+    //     <input onChange={(e) => setTask(e.currentTarget.value)} value={task} />
+    //   </label>
+    //   <button disabled={!canAdd} className={styles.button}>
+    //     Add
+    //   </button>
+    // </form>
+    <Form />
   );
 };
 
